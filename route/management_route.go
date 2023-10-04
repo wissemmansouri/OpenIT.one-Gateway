2c95471219d7e6cda59c1888b3e5c7d77071c673
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-04 13:29:28 +0200

    Create management_route.go

---
+++ route/management_route.go
@@ -0,0 +1,128 @@
+package route
+
+import (
+	"crypto/ecdsa"
+	"net/http"
+	"os"
+
+	"github.com/wissemmanouri/OpenIT.one-Common/external"
+	"github.com/wissemmansouri/OpenIT.one-Common/middleware"
+	"github.com/wissemmansouri/OpenIT.one-Common/model"
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/common_err"
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/jwt"
+	"github.com/wissemmansouri/OpenIT.one-Gateway/service"
+	"github.com/gin-contrib/gzip"
+	"github.com/gin-gonic/gin"
+)
+
+type ManagementRoute struct {
+	management *service.Management
+}
+
+func NewManagementRoute(management *service.Management) *ManagementRoute {
+	return &ManagementRoute{
+		management: management,
+	}
+}
+
+func (m *ManagementRoute) GetRoute() *gin.Engine {
+	// check if environment variable is set
+	if ginMode, success := os.LookupEnv("GIN_MODE"); success {
+		gin.SetMode(ginMode)
+	} else {
+		gin.SetMode(gin.ReleaseMode)
+	}
+
+	r := gin.Default()
+	r.Use(middleware.Cors())
+	r.Use(gzip.Gzip(gzip.DefaultCompression))
+
+	r.GET("/ping", func(ctx *gin.Context) {
+		ctx.JSON(http.StatusOK, gin.H{
+			"message": "pong from management service",
+		})
+	})
+
+	m.buildV1Group(r)
+
+	return r
+}
+
+func (m *ManagementRoute) buildV1Group(r *gin.Engine) {
+	v1Group := r.Group("/v1")
+
+	v1Group.Use()
+	{
+		m.buildV1RouteGroup(v1Group)
+	}
+}
+
+func (m *ManagementRoute) buildV1RouteGroup(v1Group *gin.RouterGroup) {
+	v1GatewayGroup := v1Group.Group("/gateway")
+
+	v1GatewayGroup.Use()
+	{
+		v1GatewayGroup.GET("/routes", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, m.management.GetRoutes())
+		})
+
+		v1GatewayGroup.POST("/routes",
+			jwt.ExceptLocalhost(func() (*ecdsa.PublicKey, error) { return external.GetPublicKey(m.management.State.GetRuntimePath()) }),
+			func(ctx *gin.Context) {
+				var route *model.Route
+				err := ctx.ShouldBindJSON(&route)
+				if err != nil {
+					ctx.JSON(http.StatusBadRequest, model.Result{
+						Success: common_err.CLIENT_ERROR,
+						Message: err.Error(),
+					})
+					return
+				}
+
+				if err := m.management.CreateRoute(route); err != nil {
+					ctx.JSON(http.StatusInternalServerError, model.Result{
+						Success: common_err.SERVICE_ERROR,
+						Message: err.Error(),
+					})
+					return
+				}
+
+				ctx.Status(http.StatusCreated)
+			})
+
+		v1GatewayGroup.GET("/port", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, model.Result{
+				Success: common_err.SUCCESS,
+				Message: common_err.GetMsg(common_err.SUCCESS),
+				Data:    m.management.GetGatewayPort(),
+			})
+		})
+
+		v1GatewayGroup.PUT("/port",
+			jwt.ExceptLocalhost(func() (*ecdsa.PublicKey, error) { return external.GetPublicKey(m.management.State.GetRuntimePath()) }),
+			func(ctx *gin.Context) {
+				var request *model.ChangePortRequest
+
+				if err := ctx.ShouldBindJSON(&request); err != nil {
+					ctx.JSON(http.StatusBadRequest, model.Result{
+						Success: common_err.CLIENT_ERROR,
+						Message: err.Error(),
+					})
+					return
+				}
+
+				if err := m.management.SetGatewayPort(request.Port); err != nil {
+					ctx.JSON(http.StatusInternalServerError, model.Result{
+						Success: common_err.SERVICE_ERROR,
+						Message: err.Error(),
+					})
+					return
+				}
+
+				ctx.JSON(http.StatusOK, model.Result{
+					Success: common_err.SUCCESS,
+					Message: common_err.GetMsg(common_err.SUCCESS),
+				})
+			})
+	}
+}
