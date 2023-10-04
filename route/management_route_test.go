61ab7c53cd5f7bd43dc5161fb50afd9cba635d69
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-04 13:39:09 +0200

    Create management_route_test.go

---
+++ route/management_route_test.go
@@ -0,0 +1,136 @@
+package route
+
+import (
+	"bytes"
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/logger"
+
+	"github.com/wissemmansouri/OpenIT.one-Common/model"
+	"github.com/wissemmansouri/OpenIT.one-Gateway/service"
+	"github.com/gin-gonic/gin"
+	"gotest.tools/v3/assert"
+)
+
+var (
+	_router *gin.Engine
+	_state  *service.State
+)
+
+func init() {
+	logger.LogInitConsoleOnly()
+}
+
+func setup(t *testing.T) func(t *testing.T) {
+	tmpdir, _ := os.MkdirTemp("", "openit-gateway-route-test")
+
+	_state = service.NewState()
+	if err := _state.SetRuntimePath(tmpdir); err != nil {
+		t.Fatal(err)
+	}
+
+	management := service.NewManagementService(_state)
+	managementRoute := NewManagementRoute(management)
+	_router = managementRoute.GetRoute()
+
+	return func(t *testing.T) {
+		management = nil
+		_router = nil
+		os.RemoveAll(tmpdir)
+	}
+}
+
+func TestPing(t *testing.T) {
+	defer setup(t)(t)
+
+	w := httptest.NewRecorder()
+
+	req, _ := http.NewRequest(http.MethodGet, "/ping", nil)
+
+	_router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+}
+
+func TestCreateRoute(t *testing.T) {
+	defer setup(t)(t)
+
+	route := &model.Route{
+		Path:   "test",
+		Target: "http://localhost:8080",
+	}
+
+	body, err := json.Marshal(route)
+	assert.NilError(t, err)
+
+	req, _ := http.NewRequest(http.MethodPost, "/v1/gateway/routes", bytes.NewReader(body))
+	req.RemoteAddr = "127.0.0.1:0"
+
+	w := httptest.NewRecorder()
+	_router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusCreated, w.Code)
+
+	req, _ = http.NewRequest(http.MethodGet, "/v1/gateway/routes", nil)
+	w = httptest.NewRecorder()
+	_router.ServeHTTP(w, req)
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var routes []*model.Route
+
+	decoder := json.NewDecoder(w.Body)
+
+	err = decoder.Decode(&routes)
+	assert.NilError(t, err)
+	assert.Equal(t, 1, len(routes))
+	assert.Equal(t, route.Path, routes[0].Path)
+	assert.Equal(t, route.Target, routes[0].Target)
+}
+
+func TestChangePort(t *testing.T) {
+	defer setup(t)(t)
+
+	actualPort := ""
+
+	_state.OnGatewayPortChange(func(s string) error {
+		actualPort = s
+		return nil
+	})
+
+	expectedPort := "123"
+
+	// set
+	request := &model.ChangePortRequest{
+		Port: expectedPort,
+	}
+
+	body, err := json.Marshal(request)
+	assert.NilError(t, err)
+
+	req, _ := http.NewRequest(http.MethodPut, "/v1/gateway/port", bytes.NewReader(body))
+	req.RemoteAddr = "127.0.0.1:0"
+
+	w := httptest.NewRecorder()
+	_router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+	assert.Equal(t, expectedPort, actualPort)
+
+	// get
+	req, _ = http.NewRequest(http.MethodGet, "/v1/gateway/port", nil)
+
+	w = httptest.NewRecorder()
+	_router.ServeHTTP(w, req)
+
+	assert.Equal(t, http.StatusOK, w.Code)
+
+	var result *model.Result
+	decoder := json.NewDecoder(w.Body)
+
+	err = decoder.Decode(&result)
+	assert.NilError(t, err)
+	assert.Equal(t, expectedPort, result.Data)
+}
