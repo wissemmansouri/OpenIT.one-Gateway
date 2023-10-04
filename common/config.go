c2d3f2834831c4486eb2384c09630fa49ce3d15a
wissemmansouri <146821020+wissemmansouri@users.noreply.github.com>
2023-10-04 13:17:44 +0200

    Create config.go

---
+++ common/config.go
@@ -0,0 +1,54 @@
+package common
+
+import (
+	"log"
+	"os"
+	"path/filepath"
+
+	"github.com/spf13/viper"
+
+	"github.com/wissemmansouri/OpenIT.one-Common/utils/constants"
+)
+
+const (
+	ConfigKeyLogPath     = "gateway.LogPath"
+	ConfigKeyLogSaveName = "gateway.LogSaveName"
+	ConfigKeyLogFileExt  = "gateway.LogFileExt"
+	ConfigKeyGatewayPort = "gateway.Port"
+	ConfigKeyRuntimePath = "common.RuntimePath"
+
+	GatewayName       = "gateway"
+	GatewayConfigType = "ini"
+)
+
+func LoadConfig() (*viper.Viper, error) {
+	config := viper.New()
+
+	config.SetDefault(ConfigKeyLogPath, constants.DefaultLogPath)
+	config.SetDefault(ConfigKeyLogSaveName, GatewayName)
+	config.SetDefault(ConfigKeyLogFileExt, "log")
+
+	config.SetDefault(ConfigKeyRuntimePath, constants.DefaultRuntimePath) // See https://refspecs.linuxfoundation.org/FHS_3.0/fhs/ch05s13.html
+
+	config.SetConfigName(GatewayName)
+	config.SetConfigType(GatewayConfigType)
+
+	if currentDirectory, err := os.Getwd(); err != nil {
+		log.Println(err)
+	} else {
+		config.AddConfigPath(currentDirectory)
+		config.AddConfigPath(filepath.Join(currentDirectory, "conf"))
+	}
+
+	if configPath, success := os.LookupEnv("CASAOS_CONFIG_PATH"); success {
+		config.AddConfigPath(configPath)
+	}
+
+	config.AddConfigPath(constants.DefaultConfigPath)
+
+	if err := config.ReadInConfig(); err != nil {
+		return nil, err
+	}
+
+	return config, nil
+}
